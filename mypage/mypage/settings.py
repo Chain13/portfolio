beb5f7ad8e069660821d311bc390b5d31f92cfbb
chain13 <chain12341@outlook.com>
2025-03-17 05:56:51 +0700

    vercel

--- mypage/mypage/settings.py
+++ mypage/mypage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4ueb1p3!$o&#1vdk!2c_!#c*$n#cpn3z+q2upv@d$&5)t9g-jm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app", "127.0.0.1"]
 
 
 # Application definition
