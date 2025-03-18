aea3c1d7716c9dafa4a0a39f45776e9e1b46e71e
chain13 <chain12341@outlook.com>
2025-03-18 09:37:54 +0700

    mypage v2

--- mypage/core/urls.py
+++ mypage/core/urls.py
@@ -1,6 +1,12 @@
 
 from django.urls import path
-from .views import home
+from .views import home, about, contact,resume,blog,portfolio
 urlpatterns = [
     path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+    path('resume/', resume, name='resume'),
+    path('blog/', blog, name='blog'),
+    path('portfolio/', portfolio, name='portfolio'),
+    
 ]
--- mypage/core/views.py
+++ mypage/core/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request, 'core/home.html')
+    return redirect('about')
+def about(request):
+    return render(request, 'core/about.html')
+def contact(request):
+    return render(request, 'core/contact.html')
+def resume(request):
+    return render(request, 'core/resume.html')
+def blog(request):
+    return render(request, 'core/blog.html')
+def portfolio(request):
+    return render(request, 'core/portfolio.html')
