a51e0902e1d68d9e7e6e8fb0f08c9ee464ddb4f0
chain13 <chain12341@outlook.com>
2025-03-25 22:57:45 +0700

    initial value create_by in admin

--- mypage/blog/admin.py
+++ mypage/blog/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from .models import Blog, Tag
+from .forms import BlogForm
 # Register your models here.
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at', 'updated_at')
     search_fields = ('title',)
+    form = BlogForm
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  # Only set created_by during creation, not on updates
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+        
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name',)
--- mypage/blog/models.py
+++ mypage/blog/models.py
@@ -7,7 +7,7 @@ class Blog(models.Model):
     subtitle = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blogs')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blogs', editable=False)
     tags = models.ManyToManyField('Tag', blank=True, related_name='blogs')
     cover = models.ImageField(upload_to='blog_covers/', blank=True, null=True)
     content = models.TextField(blank=True)
