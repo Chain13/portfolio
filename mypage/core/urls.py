ff9876a0a27fbbb66d232928b0359f03ae324faf
chain13 <chain12341@outlook.com>
2025-03-26 06:47:34 +0700

    add login page

--- mypage/core/urls.py
+++ mypage/core/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path
-from .views import home, about, contact,resume,portfolio
+from .views import home, about, contact,resume,portfolio, SignInView
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
     path('resume/', resume, name='resume'),
     path('portfolio/', portfolio, name='portfolio'),
+    path('signin/', SignInView.as_view(), name='signin'),
     
 ]
--- mypage/core/views.py
+++ mypage/core/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-
-
+from django.contrib.auth import authenticate, login
+from django.views import View
 # Create your views here.
 def home(request):
     return redirect('about')
@@ -14,3 +13,23 @@ def resume(request):
 
 def portfolio(request):
     return render(request, 'core/portfolio.html')
+
+class SignInView(View):
+    template_name = 'core/signin.html'
+    
+    def get(self, request):
+        print(request.user, request.user.is_authenticated)
+        if request.user.is_authenticated:
+            return redirect('home')
+        return render(request, self.template_name)
+    
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        
+        if user is not None:
+            login(request=request, user=user)
+            return redirect('home')
+        
+        return redirect('signin')
