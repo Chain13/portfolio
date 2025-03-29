cf2705edcff3d0db157c8c56abef1efa74c02292
chain13 <chain12341@outlook.com>
2025-03-29 21:43:44 +0700

    add error message to signin/signup

--- mypage/core/forms.py
+++ mypage/core/forms.py
@@ -1,12 +1,12 @@
 from django import forms
-
+from .validators import validate_email
 class SignInForm(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput)
     remember_me = forms.BooleanField(required=False)
 class SignUpForm(forms.Form):
     username = forms.CharField(max_length=100)
-    email = forms.EmailField()
+    email = forms.EmailField(validators=[validate_email])
     password = forms.CharField(widget=forms.PasswordInput)
     confirm_password = forms.CharField(widget=forms.PasswordInput)
     terms_and_conditions = forms.BooleanField(required=True)
--- mypage/core/views.py
+++ mypage/core/views.py
@@ -4,7 +4,7 @@ from django.views import View
 from django.contrib.auth import get_user_model
 User = get_user_model()
 from .forms import SignInForm, SignUpForm
-
+from django.db import IntegrityError
 # Create your views here.
 def home(request):
     return redirect('about')
@@ -36,8 +36,10 @@ class SignInView(View):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request=request, user=user)
-            return redirect(next or 'home')
-        return redirect(next or 'signin')
+                return redirect(next or 'home')
+            else:
+                form.add_error(None, 'Invalid username or password')
+        return render(request, self.template_name, context={'form': form})
 
     
 class SignupView(View):
@@ -57,12 +59,19 @@ class SignupView(View):
             password = form.cleaned_data.get('password')
             confirm_password = form.cleaned_data.get('confirm_password')
             terms_and_conditions = form.cleaned_data.get('terms_and_conditions')
-            if password == confirm_password and terms_and_conditions:
-                user = User.objects.create_user(username=username, password=password, email=email)
-                login(request=request, user=user)
-            return redirect(next or 'home')
+            if password == confirm_password:
+                try:
+                    user = User.objects.create_user(username=username, password=password, email=email)
+                    login(request=request, user=user)
+                    return redirect(next or 'home')
+                except IntegrityError as e:
+                    form.add_error("username", 'Username already exists')
+            else:
+                form.add_error("password", 'Passwords do not match')
+            if not terms_and_conditions:
+                form.add_error(None, 'You must agree to the terms and conditions')
     
-        return redirect(next or 'signup')
+        return render(request=request,template_name=self.template_name, context={'form': form})
 class SignOutView(View):
     def get(self, request):
         logout(request)
